50d6d7278841d4bfbaa66c20a79b82d29cc54b06
mashouyin <>
2021-03-04 13:50:32 +0800

    初始化代码提交

---
+++ Catetory/Utils.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface Utils : NSObject
+
+- (void) sayHello;
+
+@end
---
+++ Catetory/Utils.m
@@ -0,0 +1,9 @@
+#import "Utils.h"
+
+@implementation Utils
+
+- (void) sayHello{
+    NSLog(@"say hello");
+}
+
+@end
